081da903723cfc072548b3e2dff8ef37ec916871
shame-game <hoanghung.vam1209@gmail.com>
2023-10-27 12:57:25 +0700

    x

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+/* chuyển trang - nav */
 const tabct = $$('.tab-content');
 const tabit = $$('.tab-item');
 const tabActive = $(".tab-item.ac");
@@ -21,6 +22,8 @@ tabit.forEach((tab, index) => {
     }
 });
 
+/* đăng nhập / đăng ký */
+
 const bg = $('#hd2');
 const btlg = $('.account-button');
 const login = $('#login')
@@ -29,50 +32,75 @@ const sign = $('#sign')
 const alogin = $('#alogin')
 const btsg = $('.account-login')
 
-alogin.addEventListener('click',() =>{
-    login.setAttribute('style','display: flex')
-    sign.setAttribute('style','display: none')
+alogin.addEventListener('click', () => {
+    login.setAttribute('style', 'display: flex')
+    sign.setAttribute('style', 'display: none')
 });
 
-asign.addEventListener('click',() =>{
-    login.setAttribute('style','display: none')
-    sign.setAttribute('style','display: flex')
+asign.addEventListener('click', () => {
+    login.setAttribute('style', 'display: none')
+    sign.setAttribute('style', 'display: flex')
 });
 
-bg.addEventListener('click', function(){
+bg.addEventListener('click', function () {
     bg.setAttribute('style', 'display: none');
     login.setAttribute('style', 'display: none');
-    sign.setAttribute('style','display: none')
+    sign.setAttribute('style', 'display: none')
+    $('body').setAttribute('style','overflow: visible')
 });
 
-btsg.addEventListener('click', ()=>{
+btsg.addEventListener('click', () => {
     bg.setAttribute('style', 'display: flex');
     sign.setAttribute('style', 'display: flex');
+    $('body').setAttribute('style','overflow: hidden')
 })
 
-btlg.addEventListener('click', function(){
+btlg.addEventListener('click', function () {
     bg.setAttribute('style', 'display: flex');
     login.setAttribute('style', 'display: flex');
+    $('body').setAttribute('style','overflow: hidden')
 });
 
 
-/* tạo bài viết */ 
+/* tạo bài viết */
 const cre_photo = $('.create-photo')
 const cre_text = $('.create-ports')
 const cre_bg = $('.cre-bg')
 const cre_box = $('.cre-main')
 
-cre_text.addEventListener('click',()=>{
-    cre_bg.setAttribute('style','display: flex');
-    cre_box.setAttribute('style','display: flex');
+cre_text.addEventListener('click', () => {
+    cre_bg.setAttribute('style', 'display: flex');
+    cre_box.setAttribute('style', 'display: flex');
 })
 
-cre_photo.addEventListener('click',()=>{
-    cre_bg.setAttribute('style','display: flex');
-    cre_box.setAttribute('style','display: flex');
+cre_photo.addEventListener('click', () => {
+    cre_bg.setAttribute('style', 'display: flex');
+    cre_box.setAttribute('style', 'display: flex');
 })
 
-cre_bg.addEventListener('click', ()=>{
-    cre_bg.setAttribute('style','display: none');
-    cre_box.setAttribute('style','display: none');
+cre_bg.addEventListener('click', () => {
+    cre_bg.setAttribute('style', 'display: none');
+    cre_box.setAttribute('style', 'display: none');
 })
+
+/* hiện thông tin du lịch khi click */
+
+const cards = $$('.infor-card-wrap')
+const infor_main = $$('.infor-card-main')
+const clost_infor_main = $$('.close-card')
+
+cards.forEach((card, index) => {
+    const cardmain = infor_main[index];
+    card.onclick = function () {
+        cardmain.setAttribute('style', 'display: flex');
+        $('body').setAttribute('style','overflow: hidden')
+    }
+});
+
+clost_infor_main.forEach((infor_bg, index) => {
+    const infor_bg_main = infor_main[index];
+    infor_bg.onclick = function () {
+        infor_bg_main.setAttribute('style', 'display: none');
+        $('body').setAttribute('style','overflow: visible')
+    }
+});
